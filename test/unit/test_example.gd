5e0d9643353370c66144fb8f71da52931cbb77d0
Josh <8173925+joshm-3b6b58@users.noreply.github.com>
2025-12-22 20:32:46 -0800

    Fix test docstring.

--- test/unit/test_example.gd
+++ test/unit/test_example.gd
@@ -4,6 +4,6 @@ func test_passes():
 	# this test will pass because 1 does equal 1
 	assert_eq(1, 1)
 
-func test_fails():
-	# this test will fail because those strings are not equal
+func test_match_strings():
+	# this test checks in the strings are equal
 	assert_eq('hello', 'hello')
